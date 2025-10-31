393a3436657f1fbec829b64ba8a556d2a258d510
nadia120809 <nadiacahyaagstn@gmail.com>
2025-10-31 09:00:07 +0700

    push file banner

---
+++ frontend30/src/components/QuizStart.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function QuizStart() {
+  return (
+    <div className="bg-white rounded-2xl shadow-xl p-8 text-center">
+        <div className="mb-8">
+            <div className="inline-flex items-center justify-center w-24 h-24 bg-gradient-to-br from-blue-100 to-purple-100 rounded-full mb-6">
+                
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default QuizStart
---
+++ frontend30/src/components/quiz.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function quiz() {
+  return (
+    <QuizStart/>
+    // <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-py-8 px-4">
+    //     <div className="max-w4xl mx-auto mb-8">
+    //         <div className="bg-white rounded-xl shadow-lg p-6">
+    //             <div className="flex flex-col md:flex-row md:items-center md:justify-between space-y-4 md:space-y-0">
+    //                 <div className="flex-1">
+
+    //                 </div>
+
+    //             </div>
+
+    //         </div>
+
+    //     </div>
+    // </div>
+  );
+}
+
+export default quiz;
