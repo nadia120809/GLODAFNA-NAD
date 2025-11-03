f1ad915b62d277edb2b44a174a422a325a6915c8
nadia120809 <nadiacahyaagstn@gmail.com>
2025-11-03 14:09:07 +0700

    push folder gradual blur

--- frontend01/frontend01/src/App.jsx
+++ frontend01/frontend01/src/App.jsx
@@ -1,10 +1,25 @@
-import GradualBlurDemo from './components/GradualBlurDemo';
 
+import GradualBlur from '../src/components/GradualBlur';
 function App() {
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>Gradual Blur Demo</h1>
-      <GradualBlurDemo />
+      <section style={{position: 'relative',height: 500,overflow: 'hidden'}}>
+  <div style={{ height: '100%',overflowY: 'auto',padding: '6rem 2rem' }}>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy1rINDrhBb5H4T527Bq4wPtLru1I8N_6A_S25IdQ2vA&s=10"></img>
+  </div>
+
+  <GradualBlur
+    target="parent"
+    position="bottom"
+    height="6rem"
+    strength={2}
+    divCount={5}
+    curve="bezier"
+    exponential={true}
+    opacity={1}
+  />
+</section>
     </div>
   );
 }
---
+++ frontend02/frontend02/src/App.jsx
@@ -0,0 +1,32 @@
+import GradualBlur from '../src/components/GradualBlur';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <section style={{position: 'relative',height: 500,overflow: 'hidden'}}>
+  <div style={{ height: '100%',overflowY: 'auto',padding: '6rem 2rem' }}>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy1rINDrhBb5H4T527Bq4wPtLru1I8N_6A_S25IdQ2vA&s=10"></img>
+  </div>
+
+  <GradualBlur
+    target="parent"
+    position="bottom"
+    height="6rem"
+    strength={2}
+    divCount={5}
+    curve="bezier"
+    exponential={true}
+    opacity={1}
+  />
+</section>
+    </>
+  )
+}
+
+export default App
